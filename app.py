4a31f4807435b7a139249e9a2dc92ca1b47d3594
Chantapat Tancharoen <nor_illustrious@hotmail.com>
2017-04-02 20:25:37 +0700

    Fix time issue

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, abort
 from datetime import datetime
+import pytz
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    the_time = datetime.now(pytz.timezone("Asia/Bangkok")).strftime("%A, %d %b %Y %I:%M %p")
 
     return """
     <h1>Welcome!! We are Estaurant.</h1>
@@ -12,11 +15,13 @@ def homepage():
 
     """.format(time=the_time)
 
+
 @app.route('/intents/<sent>', methods=['GET'])
 def process_sentence(sent):
     if len(sent) == 0:
         abort(500)
-    return jsonify({'intent': 'keyword', 'keyword':sent})
+    return jsonify({'intent': 'keyword', 'keyword': sent})
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
