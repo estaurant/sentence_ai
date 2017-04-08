b89a4764b4d3a1cd7680787a22651871d1577fb1
Chantapat Tancharoen <nor_illustrious@hotmail.com>
2017-04-08 21:25:17 +0700

    Add Intent enum

---
+++ Intent.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Intent(Enum):
+    GREETING = "greeting"
+    GREETING_NEG = "greeting_negative"
+    EAT = "eat"
+    EAT_NEG = "eat_negative"
+    DEFAULT = "default"
